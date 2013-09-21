b7443d05bda622e409b18f324f666ff41e606964
nimbus154 <nimbus154@gmail.com>
2013-09-21 12:47:44 -0700

    Stubbed SortController and test

---
+++ src/main/java/csuf/cs544/hw1/controller/SortController.java
@@ -0,0 +1,28 @@
+package csuf.cs544.hw1.controller;
+
+/**
+ * Handles logic for the sorting feature of the application
+ *
+ */
+public class SortController {
+	
+	public SortController() { }
+	
+	/**
+	 * Controls the sorting process
+	 * @param input raw string input from user
+	 * @param displayer class used to print/log sorting process 
+	 * @return sorted array
+	 * @throws Exception if input cannot be parsed to an array of numbers
+	 * or if it violates the preconditions defined by Validator 
+	 * @see Validtor
+	 */
+	public int[] sort(String input, ArrayDisplayer displayer) 
+		throws Exception {
+		
+		
+	
+		return null;
+	}
+
+}
---
+++ src/test/java/csuf/cs544/hw1/controller/SortControllerTest.java
@@ -0,0 +1,24 @@
+package csuf.cs544.hw1.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SortControllerTest {
+	
+	private SortController controller;
+
+	@Before
+	public void setUp() throws Exception {
+		
+		controller = new SortController();
+	}
+	
+	// 
+
+	@Test
+	public void testSort() {
+		fail("Not yet implemented"); // TODO
+	}
+}
