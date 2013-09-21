11d51962f3e01f91c5697825f63abcfdf4a28153
nimbus154 <nimbus154@gmail.com>
2013-09-21 13:53:06 -0700

    Display sorted array results, step-by-step

--- src/main/java/csuf/cs544/hw1/activities/Result.java
+++ src/main/java/csuf/cs544/hw1/activities/Result.java
@@ -3,16 +3,29 @@ package csuf.cs544.hw1.activities;
 import csuf.cs544.R;
 import csuf.cs544.R.layout;
 import csuf.cs544.R.menu;
+import csuf.cs544.hw1.controller.BbSort;
+import csuf.cs544.hw1.display.ArrayDisplayer;
+import csuf.cs544.hw1.display.TextViewArrayDisplayer;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class Result extends Activity {
+	
+	private TextViewArrayDisplayer displayer;
+	private BbSort sorter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_result);
+		
+		TextView resultListTextView = (TextView) findViewById(R.id.result_list_text);
+		displayer = new TextViewArrayDisplayer(resultListTextView);
+		sorter = new BbSort();
+		
+		sorter.bubbleSort(new int[]{8, 7, 5, 3, 2, 1}, displayer);		
 	}
 
 	@Override
--- src/main/java/csuf/cs544/hw1/display/SortArrayDisplayer.java
+++
@@ -1,9 +0,0 @@
-package csuf.cs544.hw1.display;
-
-
-public class SortArrayDisplayer implements ArrayDisplayer {
-
-	public void display(int[] nums, String message) {
-		// TODO Auto-generated method stub			
-	}
-}
