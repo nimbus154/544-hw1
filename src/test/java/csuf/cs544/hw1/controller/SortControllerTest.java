ece63028b515eb9d8aa55eb0040c0e7bc4c2f1d8
nimbus154 <nimbus154@gmail.com>
2013-09-21 13:12:27 -0700

    Added SortController tests

--- src/test/java/csuf/cs544/hw1/controller/SortControllerTest.java
+++ src/test/java/csuf/cs544/hw1/controller/SortControllerTest.java
@@ -1,24 +1,51 @@
 package csuf.cs544.hw1.controller;
 
-import static org.junit.Assert.*;
-
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 public class SortControllerTest {
 	
 	private SortController controller;
+	
+	private ArrayDisplayer displayer;
 
 	@Before
 	public void setUp() throws Exception {
 		
 		controller = new SortController();
+		displayer = Mockito.mock(ArrayDisplayer.class);
+	}
+
+	/**
+	 * User supplies improperly formatted input
+	 */
+	@Test(expected = Exception.class)
+	public void testFormatFail() throws Exception {
+		
+		controller.sort("dog", null);
 	}
 	
-	// 
+	/**
+	 * User supplies invalid input
+	 */
+	@Test(expected = Exception.class)
+	public void testValidateFail() throws Exception {
+		
+		controller.sort("1 2", null);
+	}
+	
 
+	/**
+	 * User supplies improperly formatted input
+	 */
 	@Test
-	public void testSort() {
-		fail("Not yet implemented"); // TODO
+	public void testSuccess() throws Exception {
+		
+		int[] expected = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11},
+			  actual = controller.sort("2 1 3 4 5 6 7 8 9 10 11", displayer);
+		
+		Assert.assertArrayEquals(actual, expected);
 	}
 }
